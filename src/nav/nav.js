74bd299afb119b248970dd269761326615f9a5a6
priscilla-cassandra <priscilla-92@live.com>
2026-04-30 19:32:17 +0200

    changing my profile nav link so that it gets the profile for the user that is logged in

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -1,9 +1,10 @@
 import { logout } from "../auth/auth.js"
-import { getAccessToken } from "../auth/auth.js"
+
+const loggedInUser = localStorage.getItem('name')
 
 export const navLinks = [
     {name: 'Feed', href: '/html-pages/feed.html', iconClass: 'fa-regular fa-house'},
-    {name: 'MyProfile', href: '/html-pages/profile.html', iconClass: 'fa-regular fa-user'},
+    {name: 'MyProfile', href: `/html-pages/profile.html?name=${loggedInUser}`, iconClass: 'fa-regular fa-user'},
     {name: 'NewPost', href: '/html-pages/create.html', iconClass: 'fa-solid fa-plus'},
     {name: 'Logout', href: '/index.html', iconClass: 'fa-solid fa-arrow-right-from-bracket'}
 ]
@@ -26,13 +27,6 @@ export function renderNavigation(links){
                 event.preventDefault()
                 logout()
                 window.location.href = link.href
-            })
-        }
-
-        if(link.name === 'MyProfile'){
-            a.addEventListener('click', (event)=>{
-                event.preventDefault()
-
             })
         }
 
