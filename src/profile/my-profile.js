34e894a093c2b5e8df5fcc5fc17107ab8276ebf0
priscilla-cassandra <priscilla-92@live.com>
2026-05-01 11:32:36 +0200

    deletet my-profile.js as I dont use it

--- src/profile/my-profile.js
+++
@@ -1,9 +0,0 @@
-import { renderNavigation, navLinks } from "../nav/nav";
-import { isLoggedIn, siteAuthentication } from "../auth/auth.js";
-
-isLoggedIn()
-siteAuthentication()
-
-const navigation = document.getElementById('sidebar-navigation')
-const navbar = renderNavigation(navLinks)
-navigation.appendChild(navbar)
