979c5aec43a429fb31a22e17f4965694df6f54a4
priscilla-cassandra <priscilla-92@live.com>
2026-05-01 11:33:54 +0200

    all edit logic created! Get post to edit, populate input fields with the content of the post, put request to update post, and function to submit the updated post

--- src/profile/edit.js
+++ src/profile/edit.js
@@ -1,3 +1,4 @@
+import { put, get } from "../auth/apiClient.js";
 import { renderNavigation, navLinks } from "../nav/nav.js";
 import { logout, isLoggedIn, siteAuthentication } from "../auth/auth.js";
 
@@ -7,3 +8,72 @@ siteAuthentication()
 const navigation = document.getElementById('sidebar-navigation')
 const navbar = renderNavigation(navLinks)
 navigation.appendChild(navbar)
+
+const editForm = document.getElementById('edit-form')
+const imageUrlInput = document.getElementById('edit-imageURL')
+const titleInput = document.getElementById('edit-title')
+const captionInput = document.getElementById('edit-caption')
+const updatePostMessage = document.getElementById('update-post-message')
+
+const params = new URLSearchParams(window.location.search)
+const id = params.get('id')
+
+async function getPostToEdit(){
+    try{
+        const postToEdit = await get(`/social/posts/${id}?_author=true`)
+        const result = postToEdit.data
+
+        console.log(result) //REMOVE
+
+        if(result.media?.url){
+            imageUrlInput.value = result.media.url
+        }
+
+        titleInput.value = result.title
+
+        if(result.body){
+            captionInput.value = result.body
+        }
+
+    }catch(error){
+        console.error(error.message)
+        editForm.textContent = 'Could not get the post you want to edit. Please try again'
+    }
+}
+
+async function updatePost(id){
+    const requestBody = {
+        title: titleInput.value,
+        body: captionInput.value,
+    }
+
+    if(imageUrlInput.value){
+        requestBody.media = {
+            url: imageUrlInput.value,
+            alt: titleInput.value
+        }
+    }
+
+    try{
+        
+        await put(`/social/posts/${id}`, requestBody)
+        
+        updatePostMessage.textContent = 'Your post was updated! Lets have a look at it...'
+        setTimeout(()=>{
+            window.location.href = `/html-pages/post.html?id=${id}`
+        }, 2000)
+
+    }catch(error){
+        console.error(error.message)
+        updatePostMessage.textContent = 'There was a problem with editing your post. Please try again!'
+    }
+}
+
+function submitUpdatePostForm(event){
+    event.preventDefault()
+    updatePost(id)
+
+}
+
+getPostToEdit()
+editForm.addEventListener('submit', submitUpdatePostForm)
